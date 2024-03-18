edff8f6ad40d1dfee59f9b6204da33ba9bb9315e
Khoirul Hidayah <rullhida23@gmail.com>
2024-03-18 12:21:32 +0700

    Pertemuan 5 - Fungsi

--- praktik_php/fungsi.php
+++ praktik_php/fungsi.php
@@ -88,27 +88,6 @@
         
         //memanggil fungsi perkenalan
         Perkenalan4("Elok");
-
-        echo "<br><br>";
-        echo "--Fungsi Rekrusif--<br>";
-        echo "<br>";
-
-        //function tampilkanHaloDunia(){
-        //    echo "Halo Dunia! <br>";
-
-        //    tampilkanHaloDunia();
-        //}
-        //tampilkanHaloDunia();
-
-        function tampilkanAngka(int $jumlah, int $indeks = 1){
-            echo "Perulangan ke-($indeks) <br>";
-
-            //panggil diri sendiri selama $indeks <= $jumlah
-            if ($indeks < $jumlah){
-                tampilkanAngka($jumlah, $indeks+1);
-            }
-        }
-        tampilkanAngka(20);    
     ?>
     </body>
 </html>
